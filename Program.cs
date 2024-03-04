0367ab5df109c6505d59053d4e366e34cda7c608
Zoey Kennedy <339523656@GoTVDSB.ca>
2024-03-04 13:16:04 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿namespace _5._0_Exercises
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int Person1;
+            int Person2;
+            int Person3;
+
+
+            Console.WriteLine("What is the age of Person 1?");
+            Person1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Person 1 is "+ Person1 + " years old.");
+            Console.WriteLine("What is the age of Person 2?");
+            Person2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Person 1 is " + Person2 + " years old.");
+            Console.WriteLine("What is the age of Person 3?");
+            Person3 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Person 3 is " + Person3 + " years old.");
+
+
+
+        }
+
+    }
+}
